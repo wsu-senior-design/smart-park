0d8348e4159b98cc94b70c27dc70c89c0c5aae0c
Tuan <8826tuan@gmail.com>
2022-03-03 18:30:09 -0600

    Added symbols for taken and open spots

---
+++ smart-park/spotTaken.swift
@@ -0,0 +1,19 @@
+//
+//  spotTaken.swift
+//  smart-park
+//
+//  Created by Tuan Nguyen on 3/3/22.
+//
+
+import SwiftUI
+
+struct spotTaken: View {
+    var body: some View {
+        
+        Image(systemName: "x.square.fill")
+            .renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original))
+            .resizable()
+                .frame(width: 30, height: 30)
+                .offset(y: -10)
+    }
+}
